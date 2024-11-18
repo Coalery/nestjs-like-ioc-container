b66c4b5f47cb20dbe237c6964b1ff78221d6ead7
Coalery <doralife12@gmail.com>
2024-11-18 23:39:24 +0900

    feat: 생성자 프로바이더 및 클래스 프로바이더 구현

--- src/common/Provider.ts
+++ src/common/Provider.ts
@@ -1,8 +1,19 @@
-import { Token } from './Token';
+import { Token } from '@lery/common/Token';
+import { Type } from '@lery/common/Type';
 
-export type Provider<T = any> = ValueProvider<T>;
+export type Provider<T = any> =
+  | ConstructorProvider<T>
+  | ValueProvider<T>
+  | ClassProvider<T>;
+
+export type ConstructorProvider<T = any> = Type<T>;
 
 export type ValueProvider<T = any> = {
   provide: Token;
   useValue: T;
+};
+
+export type ClassProvider<T = any> = {
+  provide: Token;
+  useClass: Type<T>;
 };
