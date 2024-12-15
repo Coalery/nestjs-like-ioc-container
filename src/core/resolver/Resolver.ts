958d72d9fd79110a5166cda26f14ae56a5dbeb72
Coalery <doralife12@gmail.com>
2024-12-16 00:49:39 +0900

    feat: 동적 인스턴스 의존성 리졸버 구현

--- src/core/resolver/Resolver.ts
+++ src/core/resolver/Resolver.ts
@@ -1,53 +1,143 @@
-import { Provider } from '@lery/common/Provider';
+import { ClassProvider, Provider, ValueProvider } from '@lery/common/Provider';
 import { Token } from '@lery/common/Token';
-import { Type } from '@lery/common/Type';
+
 import {
   PARAMETER_INJECT_METADATA,
   PROPERTY_INJECT_METADATA,
   ParameterInjectMetadata,
   PropertyInjectMetadata,
 } from '@lery/core/metadata/InjectMetadata';
-import { InstanceContainer } from '../container/InstanceContainer';
+import { InstanceContainer } from '@lery/core/container/InstanceContainer';
+import { InstanceWrapper } from '@lery/core/instance/InstanceWrapper';
+
+type PropertyResolveResult = {
+  propertyKey: string | symbol;
+  instance: any;
+};
 
 export class Resolver {
   private readonly instanceContainer: InstanceContainer;
 
-  constructor() {
+  constructor(providers: Provider[]) {
     this.instanceContainer = new InstanceContainer();
+    this.registerProviders(providers);
   }
 
-  registerProviders(providers: Provider[]): void {
+  private registerProviders(providers: Provider[]): void {
     providers.forEach((provider) => {
-      this.instanceContainer.register(provider.provide, provider.useValue);
+      if ('useValue' in provider) {
+        this.registerValueProvider(provider);
+      } else if ('useClass' in provider) {
+        this.registerClassProvider(provider);
+      } else {
+        this.registerClassProvider({ provide: provider, useClass: provider });
+      }
     });
   }
 
-  resolve<T>(ctor: Type<T>): T {
-    const parameters = this.resolveConstructorParameters(ctor);
-    const instance = new ctor(...parameters);
-    this.resolveProperty(ctor, instance);
+  private registerValueProvider<T>(provider: ValueProvider<T>): void {
+    const token = provider.provide;
+    const instance = provider.useValue;
 
-    return instance;
+    const wrapper = new InstanceWrapper(token);
+    wrapper.resolve(instance);
+
+    this.instanceContainer.register(token, wrapper);
   }
 
-  resolveConstructorParameters<T>(ctor: Type<T>): any[] {
+  private registerClassProvider<T>(provider: ClassProvider<T>): void {
+    const token = provider.provide;
+    const ctor = provider.useClass;
+
+    const wrapper = new InstanceWrapper(token, ctor);
+
+    this.instanceContainer.register(token, wrapper);
+  }
+
+  resolve(): void {
+    this.instanceContainer.wrappers.forEach((wrapper) => {
+      this.loadProvider(wrapper);
+    });
+  }
+
+  get<T>(token: Token<T>): T {
+    const wrapper = this.instanceContainer.get(token);
+
+    if (wrapper.instance === undefined) {
+      throw new Error(
+        `Instance of ${wrapper.token.toString()} is not resolved`
+      );
+    }
+
+    return wrapper.instance;
+  }
+
+  private loadProvider<T>(wrapper: InstanceWrapper<T>): void {
+    if (wrapper.isResolved) {
+      return;
+    }
+
+    const ctorParams = this.resolveConstructorParameters(wrapper);
+    const propParams = this.resolveProperties(wrapper);
+
+    const instance = this.instantiate(wrapper, ctorParams);
+    this.applyProperties(instance, propParams);
+
+    wrapper.resolve(instance);
+  }
+
+  private resolveConstructorParameters<T>(wrapper: InstanceWrapper<T>): any[] {
+    if (!wrapper.creatorFn) {
+      return [];
+    }
+
     const metadata: ParameterInjectMetadata[] =
-      Reflect.getMetadata(PARAMETER_INJECT_METADATA, ctor) ?? [];
+      Reflect.getMetadata(PARAMETER_INJECT_METADATA, wrapper.creatorFn) ?? [];
 
     const result: any[] = [];
-    metadata.map((m) => {
-      result[m.index] = this.instanceContainer.get(m.token);
+    metadata.forEach((m) => {
+      const wrapper = this.instanceContainer.get(m.token);
+      this.loadProvider(wrapper);
+
+      result[m.index] = wrapper.instance;
     });
 
     return result;
   }
 
-  resolveProperty<T>(ctor: Type<T>, instance: T): void {
-    const metadata: PropertyInjectMetadata[] =
-      Reflect.getMetadata(PROPERTY_INJECT_METADATA, ctor) ?? [];
+  private resolveProperties<T>(
+    wrapper: InstanceWrapper<T>
+  ): PropertyResolveResult[] {
+    if (!wrapper.creatorFn) {
+      return [];
+    }
 
-    metadata.forEach((m) => {
-      (instance as any)[m.propertyKey] = this.instanceContainer.get(m.token);
+    const metadata: PropertyInjectMetadata[] =
+      Reflect.getMetadata(PROPERTY_INJECT_METADATA, wrapper.creatorFn) ?? [];
+
+    const result = metadata.map<PropertyResolveResult>((m) => {
+      const wrapper = this.instanceContainer.get(m.token);
+      this.loadProvider(wrapper);
+
+      return { propertyKey: m.propertyKey, instance: wrapper.instance };
     });
+
+    return result;
+  }
+
+  private applyProperties<T>(
+    instance: T,
+    properties: PropertyResolveResult[]
+  ): void {
+    properties.forEach((p) => {
+      (instance as any)[p.propertyKey] = p.instance;
+    });
+  }
+
+  private instantiate<T>(wrapper: InstanceWrapper<T>, ctorParams: any[]): T {
+    const instance: T = new wrapper.creatorFn!(...ctorParams);
+    wrapper.resolve(instance);
+
+    return instance;
   }
 }
