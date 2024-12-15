5d92c319dda9fa6e07603375b3100004d333fc96
Coalery <doralife12@gmail.com>
2024-12-16 00:50:00 +0900

    feat: 새로운 구현체에 맞게 예시 코드 수정

--- src/main.ts
+++ src/main.ts
@@ -15,10 +15,6 @@ import {
   HelloWorldToken,
   IHelloWorld,
 } from '@lery/instances/helloWorld/IHelloWorld';
-import {
-  FileManagerToken,
-  IFileManager,
-} from '@lery/instances/fileManager/IFileManager';
 
 class Hello {
   @Inject(CalculatorToken)
@@ -30,26 +26,42 @@ class Hello {
   constructor(
     @Inject(HelloWorldToken)
     readonly helloWorld: IHelloWorld,
-    @Inject(FileManagerToken)
-    readonly fileManager: IFileManager
+    @Inject(FileManager)
+    readonly fileManager: FileManager
   ) {}
 }
 
+class Something {
+  constructor(
+    @Inject(Hello)
+    private readonly hello: Hello
+  ) {}
+
+  addOneAndTwo(): number {
+    return this.hello.calculator.add(1, 2);
+  }
+}
+
 function main() {
-  const resolver = new Resolver();
-  resolver.registerProviders([
+  const resolver = new Resolver([
     { provide: CalculatorToken, useValue: new Calculator() },
     { provide: LoggerToken, useValue: new Logger() },
-    { provide: HelloWorldToken, useValue: new HelloWorld() },
-    { provide: FileManagerToken, useValue: new FileManager() },
+    { provide: HelloWorldToken, useClass: HelloWorld },
+    FileManager,
+    { provide: Hello, useClass: Hello },
+    Something,
   ]);
+  resolver.resolve();
 
-  const hello = resolver.resolve(Hello);
+  const hello = resolver.get(Hello);
 
   console.log(hello.calculator.add(1, 2)); // 3
   hello.logger.log('Hello from logger'); // Hello from logger
   hello.helloWorld.sayHello(); // Hello, World!
   hello.fileManager.write('some.txt', 'Hello!'); // Writing file to some.txt with content: Hello!
+
+  const something = resolver.get(Something);
+  console.log(something.addOneAndTwo()); // 3
 }
 
 main();
